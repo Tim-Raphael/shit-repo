349ebc461c328a7966eb596a21b19b85d4865da5
Tim++ <120750985+Tim-Raphael@users.noreply.github.com>
2023-01-21 17:51:23 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,31 @@
 // SET SOME BODY CLASSES //////
-////////////////////////////// 
-let documentName = document.location.pathname.split('/').pop().replace('.php', '');
-document.body.classList.add(documentName);
+//////////////////////////////
+
+let documentName = document.location.pathname;
+
+if (documentName.trim() === '/') {
+  let documentName = 'index';
+  document.body.classList.add(documentName);
+}else {
+  let documentName = document.location.pathname.split('/').pop().replace('.php', '');
+  document.body.classList.add(documentName);
+}
+
+function atTheTop() {
+  let scrollTop = window.pageYOffset;
+
+  if (scrollTop < 50) {
+    document.body.classList.add('atTheTop');
+  } else {
+    document.body.classList.remove('atTheTop');
+  }
+};
+
+atTheTop();
 
 window.addEventListener('wheel', function() {
+  atTheTop();
+
   if (checkScrollDirectionIsUp(event)) {
     document.body.classList.remove('scrollingDown');
     document.body.classList.add('scrollingUp');
@@ -11,6 +33,7 @@ window.addEventListener('wheel', function() {
     document.body.classList.remove('scrollingUp');
     document.body.classList.add('scrollingDown');
   }
+
   function checkScrollDirectionIsUp(event) {
     if (event.wheelDelta) {
       return event.wheelDelta > 0;
